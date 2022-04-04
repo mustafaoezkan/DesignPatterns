320438bfbe2b0d0fab04475aa0df2449cec7cf94
Mustafa Özkan <1030522916@erciyes.edu.tr>
2022-04-04 14:21:17 +0300

    abstract factory

--- factory-method.ts
+++ factory-method.ts
@@ -36,14 +36,14 @@ class ConcreteProduct2 implements Product {
     }
 }
 
-function clientCode(creator: Creator) {
+function clientCodeForFactory(creator: Creator) {
     console.log('Client: I\'m not aware of the creator\'s class, but it still works.');
     console.log(creator.someOperation());
 }
 
 console.log('App: Launched with the ConcreteCreator1.');
-clientCode(new ConcreteCreator1());
+clientCodeForFactory(new ConcreteCreator1());
 console.log('');
 
 console.log('App: Launched with the ConcreteCreator2.');
-clientCode(new ConcreteCreator2());
+clientCodeForFactory(new ConcreteCreator2());
